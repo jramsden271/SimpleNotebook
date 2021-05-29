773554186e189db89ce5ded3029f439200ddeb2c
James Ramsden <jramsden271@gmail.com>
2021-05-29 17:38:07 +0100

    savedammit

--- SimpleNotebook/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ SimpleNotebook/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -117,7 +117,7 @@ namespace SimpleNotebook {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SimpleNotebook;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SimpleNotebook;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
